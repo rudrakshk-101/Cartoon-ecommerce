f0aa15f929048e49d67e7a51795ba99ebe8e17a9
Rudraksh Kumrawat <83749855+rudrakshk-101@users.noreply.github.com>
2024-02-16 14:51:05 +0530

    page creation

---
+++ www.cartoon.com/src/pages/checkoutPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CheckoutPage = ()=>{
+    return(
+        <h1>Check out page</h1>
+    )
+}
+
+export default CheckoutPage
---
+++ www.cartoon.com/src/pages/paymentsPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PaymentsPage = ()=>{
+    return(
+        <h1>payment page</h1>
+    )
+}
+
+export default PaymentsPage;
