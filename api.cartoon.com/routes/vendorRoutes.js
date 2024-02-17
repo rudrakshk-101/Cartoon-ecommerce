406582811ab56b4079a3668c559c39f5b4bfd784
Aayush Joshi <76811479+Aayush-Joshi-01@users.noreply.github.com>
2024-02-17 17:02:40 +0530

    Deletion

--- api.cartoon.com/routes/vendorRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const vendorController = require('../controllers/vendorController');
-
-router.post('/register',vendorController.register);
-router.post('/login',vendorController.login);
-router.post('/getVendor',vendorController.getVendor);
-
-module.exports = router;
